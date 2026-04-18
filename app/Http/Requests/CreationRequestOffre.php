b369831e445cfd9a576e8761a4344b32d087b173
Nait Abderrahmane Hamza <naitabderrahmane.hamza@ensam-casa.ma>
2026-04-18 18:03:39 +0100

    feat: add offer management with filters, pagination and recruiter access control

---
+++ app/Http/Requests/CreationRequestOffre.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreationRequestOffre extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+        'titre'=>'required|string|min:4',
+        'description'=>'nullable|string',
+        'localisation'=>'required|string',
+        'type'=>'string|in:CDI,CDD,stage',
+        'actif'=>'booalean|string'
+        ];
+    }
+}
